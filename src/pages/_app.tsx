74990fa213d5bb03cc270ab0e995ec13a1d618c0
James Wallington <jameswallington11@gmail.com>
2024-04-18 15:55:44 +0200

    WIP: mongo connection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,7 @@ import "../styles/globals.css";
 import { trpc } from "@/utils/trpc";
 import { FormProvider, useForm } from "react-hook-form";
 import { SessionProvider, getSession, useSession } from "next-auth/react";
-import { authOptions, getServerAuthSession } from "@/server/auth";
-import { NextPage } from "next";
-import { Session } from "next-auth";
+
 import Navbar from "@/components/Navbar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,17 @@
 import { appRouter } from "@/server/root";
 import { createTRPCContext } from "@/server/trpc";
 import * as trpcNext from "@trpc/server/adapters/next";
+import { env } from "process";
 
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
+  onError:
+    env.NODE_ENV === "development"
+      ? ({ path, error }) => {
+          console.error(
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+          );
+        }
+      : undefined,
 });
